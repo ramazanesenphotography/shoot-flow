bb186dabeaed316e6a8080bc0fa672e0515b16e7
ramazanesenphotography <ramazanesen23@gmail.com>
2026-07-26 05:36:28 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,9 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://bvvkulcsmidbvvouthey.supabase.co';
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_rxVMJPfJQcHrUha4wEMCgg_dtBRGInm';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://bvvkulcsmidbvvouthey.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_rxVMJPfJQcHrUha4wEMCgg_dtBRGInm';
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
